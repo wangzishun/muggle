35215726e71d91258d2157e40134941d793e5f40
wangzi <nice.to.meet.wangzi@gmail.com>
2023-10-01 04:33:42 +0000

    upgrade: pkg

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,22 @@
-import nextMdx from '@next/mdx'
+// import nextMdx from '@next/mdx'
 
-const withMdx = nextMdx({
-  // By default only the .mdx extension is supported.
-  extension: /\.mdx?$/,
-  options: {
-    /* providerImportSource: …, otherOptions… */
-    // providerImportSource: '@mdx-js/react',
-  },
-})
+// const withMdx = nextMdx({
+//   // By default only the .mdx extension is supported.
+//   extension: /\.mdx?$/,
+//   options: {
+//     /* providerImportSource: …, otherOptions… */
+//     // providerImportSource: '@mdx-js/react',
+//   },
+// })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    mdxRs: true,
-  },
-  // TODO: 无法使用 output: export, 等待bug修复 https://github.com/vercel/next.js/issues/47334
-  // output: 'export',
+  experimental: {},
+  output: 'export',
   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
   compiler: {
     // removeConsole: false
-  }
+  },
 }
 
-export default withMdx(nextConfig)
+export default nextConfig
